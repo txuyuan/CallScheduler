7bcfba2562d086f3f327b5fc4ce2a86e894b9da8
txuyuan <191519R@student.hci.edu.sg>
2026-09-26 23:50:38 +0800

    fix(teams_calls): fix run script timeout

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd /Users/txuyuan/Dev/pythonProjects/CallScheduler
 /Users/txuyuan/Dev/pythonProjects/CallScheduler/.venv/bin/python /Users/txuyuan/Dev/pythonProjects/CallScheduler/src/teams_calls/excel.py
 echo "Press enter to exit..."
-read -t 30
+read
